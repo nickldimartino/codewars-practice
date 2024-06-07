fc03e80ae1e6acf23072590470dc07197aaeadbd
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-06-07 08:19:45 -0400

    Update StopgninnipSMysdroW!.ts

--- typescript/6kyu/StopgninnipSMysdroW!.ts
+++ typescript/6kyu/StopgninnipSMysdroW!.ts
@@ -1,5 +1,7 @@
 /*
-Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more 
+letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included 
+only when more than one word is present.
 */
 
 export function spinWords(words: string): string {
