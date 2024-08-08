9d1762116ee90df5588eb2bef8a3480d965fab54
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-08-08 10:53:57 -0400

    Create OppositeNumber.cpp
    Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).

---
+++ c++/8kyu/OppositeNumber.cpp
@@ -0,0 +1,8 @@
+/*
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+*/
+
+int opposite(int number) 
+{
+  return (-1) * number;
+}
