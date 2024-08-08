73793ced0436d3b3ad60c4ac549b93110db8493f
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-08-08 10:22:11 -0400

    Create EvenOrOdd.cpp
    Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.

---
+++ c++/EvenOrOdd.cpp
@@ -0,0 +1,13 @@
+/* 
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+#include <string>
+
+std::string even_or_odd(int number){
+  if (number % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
